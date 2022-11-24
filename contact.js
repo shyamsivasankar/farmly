5ce1638782a4224bd12ba8c1c8eda868b25de170
Zitharth <89957512+Zitharth@users.noreply.github.com>
2022-11-24 15:24:04 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -3,13 +3,13 @@ function sendEmail(){
         Host : "smtp.gmail.com",
         Username : "afiitjeeboy.zr@gmail.com",
         Password : "11CE70875072F26FEB5D3FB06D9D8479A06B",
-        To : 'afiitjeeboy.zr@gmail.com',
+        To : '20pd40c1@gmail.com',
         From : document.getElementById("email").value,
         Subject : "Regarding farmly",
         Body : "Name: " + document.getElementById("name").value
-            + "Email: " + document.getElementById("email").value
-            + "Phone number: " + document.getElementById("pnum").value
-            + "Comments: " + document.getElementById("msg").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Phone number: " + document.getElementById("pnum").value
+            + "<br> Comments: " + document.getElementById("msg").value
     }).then(
       message => alert("Mail has been sent successfully")
     );
