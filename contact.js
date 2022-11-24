84c8019ee8cc96f6913e47f5b75e214dc7ee8d4b
shyamsivasankar <shyamshivshankar33@gmail.com>
2022-11-24 16:24:24 +0530

    small changes

--- contact.js
+++ contact.js
@@ -4,7 +4,7 @@ function sendEmail(){
         Username : "afiitjeeboy.zr@gmail.com",
         Password : "11CE70875072F26FEB5D3FB06D9D8479A06B",
         To : '20pd40c1@gmail.com',
-        From : document.getElementById("email").value,
+        From : "afiitjeeboy.zr@gmail.com",
         Subject : "Regarding farmly",
         Body : "Name: " + document.getElementById("name").value
             + "<br> Email: " + document.getElementById("email").value
