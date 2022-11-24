5699eb960e94997174bafa2ff4eb3662ca41fb1e
Zitharth <89957512+Zitharth@users.noreply.github.com>
2022-11-24 15:19:03 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -2,8 +2,8 @@ function sendEmail(){
     Email.send({
         Host : "smtp.gmail.com",
         Username : "afiitjeeboy.zr@gmail.com",
-        Password : "Un!versit0",
-        To : '20pd40c1@gmail.com',
+        Password : "11CE70875072F26FEB5D3FB06D9D8479A06B",
+        To : 'afiitjeeboy.zr@gmail.com',
         From : document.getElementById("email").value,
         Subject : "Regarding farmly",
         Body : "Name: " + document.getElementById("name").value
